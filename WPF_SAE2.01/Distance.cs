350c96d2dff021a5836a8ed3deec84127eff6274
levetle <levetle@D361-S21>
2024-06-04 15:59:52 +0200

    recuperation envoie sms fini

--- WPF_SAE2.01/Distance.cs
+++ WPF_SAE2.01/Distance.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +32,72 @@ namespace WPF_SAE2._01
             this.NumCourse = numCourse;
             this.NumBorne = numBorne;
             this.Nb_Km = nb_Km;
+        }
+        public static ObservableCollection<Distance> Read()
+        {
+            ObservableCollection<Distance> lesDistances = new ObservableCollection<Distance>();
+            string sql = "SELECT * FROM schemasae201.ami";
+            DataTable dt = DataAccess.Instance.GetData(sql);
+            Console.WriteLine(dt);
+
+            if (dt != null)
+            {
+                foreach (DataRow res in dt.Rows)
+                {
+                    try
+                    {
+                        Course numCourse = (Course)res["num_course"];
+                        Borne numBorne = (Borne)res["num_borne"];
+                        int nb_Km = int.Parse(res["nb_km"].ToString());
+                        Distance nouveau = new Distance(numCourse, numBorne, nb_Km);
+                        lesDistances.Add(nouveau);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Error parsing DataRow: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("DataTable is null. Check database connection and query.");
+            }
+
+            return lesDistances;
+        }
+        public static List<Course> ReadNumCourse()
+        {
+            ObservableCollection<Distance> lesDistances = Read();
+            List<Course> numCourses = new List<Course>();
+            foreach (Distance uneDistance in lesDistances)
+            {
+                numCourses.Add(uneDistance.NumCourse);
+
+            }
+            return numCourses;
+        }
+        public static List<Borne> ReadNumBorne()
+        {
+            ObservableCollection<Distance> lesDistances = Read();
+            List<Borne> numBornes = new List<Borne>();
+            foreach (Distance uneDistance in lesDistances)
+            {
+                numBornes.Add(uneDistance.NumBorne);
+
+            }
+            return numBornes;
+        }
+        public static List<int> ReadNb_Km()
+        {
+            ObservableCollection<Distance> lesDistances = Read();
+            List<int> nbs_Kms = new List<int>();
+            foreach (Distance uneDistance in lesDistances)
+            {
+                nbs_Kms.Add(uneDistance.nb_Km);
+
+            }
+            return nbs_Kms;
         }
     }
 }
