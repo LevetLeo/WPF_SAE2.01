1bc2db73cca499ee5c6d35db64289e2871e6d033
Mattheo Guzman <Mattheo.Guzman@etu.univ-smb.fr>
2024-06-07 14:17:27 +0200

    federation filtre en cours

--- WPF_SAE2.01/Coureur.xaml.cs
+++ WPF_SAE2.01/Coureur.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +28,8 @@ namespace WPF_SAE2._01
         {
             
             InitializeComponent();
+            
+            
             ObservableCollection<CoureurClasse>  lesCoureurs = CoureurClasse.Read();
             DataGridCoureur.ItemsSource = lesCoureurs;
 
@@ -33,15 +37,60 @@ namespace WPF_SAE2._01
 
             
         }
+        public bool ContientMotCleNom(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(TBSelNom.Text))
+                return true;
+            else
+                return (unCoureur.NomCoureur.StartsWith(TBSelNom.Text, StringComparison.OrdinalIgnoreCase));
 
+        }
+        public bool ContientMotCleVille(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(TBSelVille.Text))
+                return true;
+            else
+                return (unCoureur.VilleCoureur.StartsWith(TBSelVille.Text, StringComparison.OrdinalIgnoreCase));
+
+        }
         private void TBSelNom_TextChanged(object sender, TextChangedEventArgs e)
         {
+            DataGridCoureur.Items.Filter = ContientMotCleNom;
             CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
 
         }
 
         private void TBSelVille_TextChanged(object sender, TextChangedEventArgs e)
         {
+            DataGridCoureur.Items.Filter = ContientMotCleVille;
+            CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
+
+        }
+        public bool ContientMotCleFederation(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(ComboBoxFederation.Text))
+                return true;
+            else
+                return (unCoureur.Federation.StartsWith(ComboBoxFederation.Text, StringComparison.OrdinalIgnoreCase));
+
+        }
+
+
+
+
+
+
+            
+            
+        private void ComboBoxFederation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+
+
+            DataGridCoureur.Items.Filter = ContientMotCleFederation;
             CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
 
         }
