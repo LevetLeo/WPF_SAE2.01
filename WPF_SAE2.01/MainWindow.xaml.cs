cdf8a64a0c22e9a756bd0ec6f5f1a832cc486561
leo levet <leo.levet@etu.univ-smb.fr>
2024-06-09 21:03:24 +0200

    meilleur interface graphique MainWindow

--- WPF_SAE2.01/MainWindow.xaml.cs
+++ WPF_SAE2.01/MainWindow.xaml.cs
@@ -57,37 +57,57 @@ namespace WPF_SAE2._01
             this.DataContext = data;
             InitializeComponent();
             int i = 0;
-            
+            ObservableCollection<CoureurClasse> lesCoureurs = CoureurClasse.Read();
+            DataGridCoureur.ItemsSource = lesCoureurs;
+
 
 
             ListeOBJETCourse = Course.Read();
             Console.WriteLine(ListeOBJETCourse.Count);
-            LabelCourse.Content = ListeOBJETCourse[0 + i].NomCourse + "\n Distance \n"
-            + ListeOBJETCourse[0 + i].Distance + "\n DateHeure: \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
-            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year +
-            "\n Heure depart: \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
-            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute + "\n Prix: \n" +
+            LabelCourse.Content = ListeOBJETCourse[0 + i].NomCourse;
+            DistanceCourse.Content = "\n Distance : \n" + ListeOBJETCourse[0 + i].Distance;
+            DateCourse.Content = "\n Date de départ : \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
+            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year;
+            HeureCourse.Content = "\n Heure depart : \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
+            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute;
+            PrixCourse.Content = "\n Prix: \n" +
             ListeOBJETCourse[0 + i].PrixInscriptionCourse;
 
-            LabelCourse2.Content = ListeOBJETCourse[1 + i].NomCourse + "\n Distance \n"
-            + ListeOBJETCourse[1 + i].Distance + "\n DateHeure: \n" + ListeOBJETCourse[1 + i].DateDepart.Day + "/" + ListeOBJETCourse[1 + i].DateDepart.Month + "/" + ListeOBJETCourse[1 + i].DateDepart.Year +
-            "\n Heure depart: \n " + ListeOBJETCourse[1 + i].HeureDepartCourse.Hour + ":" + ListeOBJETCourse[1 + i].HeureDepartCourse.Minute + "\n Prix: \n" +
-            ListeOBJETCourse[1 + i].PrixInscriptionCourse;
+            LabelCourse2.Content = ListeOBJETCourse[0 + i].NomCourse;
+            DistanceCourse2.Content = "\n Distance : \n" + ListeOBJETCourse[0 + i].Distance;
+            DateCourse2.Content = "\n Date de départ : \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
+            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year;
+            HeureCourse2.Content = "\n Heure depart : \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
+            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute;
+            PrixCourse2.Content = "\n Prix: \n" +
+            ListeOBJETCourse[0 + i].PrixInscriptionCourse;
 
-            LabelCourse3.Content = ListeOBJETCourse[2 + i].NomCourse + "\n Distance \n"
-            + ListeOBJETCourse[2 + i].Distance + "\n DateHeure: \n" + ListeOBJETCourse[2 + i].DateDepart.Day + "/" + ListeOBJETCourse[2 + i].DateDepart.Month + "/" + ListeOBJETCourse[2 + i].DateDepart.Year +
-            "\n Heure depart: \n " + ListeOBJETCourse[2 + i].HeureDepartCourse.Hour + ":" + ListeOBJETCourse[2 + i].HeureDepartCourse.Minute + "\n Prix: \n" +
-            ListeOBJETCourse[2 + i].PrixInscriptionCourse;
+            LabelCourse3.Content = ListeOBJETCourse[0 + i].NomCourse;
+            DistanceCourse3.Content = "\n Distance : \n" + ListeOBJETCourse[0 + i].Distance;
+            DateCourse3.Content = "\n Date de départ : \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
+            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year;
+            HeureCourse3.Content = "\n Heure depart : \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
+            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute;
+            PrixCourse3.Content = "\n Prix: \n" +
+            ListeOBJETCourse[0 + i].PrixInscriptionCourse;
 
-            LabelCourse4.Content = ListeOBJETCourse[3 + i].NomCourse + "\n Distance \n"
-            + ListeOBJETCourse[3 + i].Distance + "\n DateHeure: \n" + ListeOBJETCourse[3 + i].DateDepart.Day + "/" + ListeOBJETCourse[3 + i].DateDepart.Month + "/" + ListeOBJETCourse[3 + i].DateDepart.Year +
-            "\n Heure depart: \n " + ListeOBJETCourse[3 + i].HeureDepartCourse.Hour + ":" + ListeOBJETCourse[3 + i].HeureDepartCourse.Minute + "\n Prix: \n" +
-            ListeOBJETCourse[3 + i].PrixInscriptionCourse;
+            LabelCourse4.Content = ListeOBJETCourse[0 + i].NomCourse;
+            DistanceCourse4.Content = "\n Distance : \n" + ListeOBJETCourse[0 + i].Distance;
+            DateCourse4.Content = "\n Date de départ : \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
+            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year;
+            HeureCourse4.Content = "\n Heure depart : \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
+            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute;
+            PrixCourse4.Content = "\n Prix: \n" +
+            ListeOBJETCourse[0 + i].PrixInscriptionCourse;
 
-            LabelCourse5.Content = ListeOBJETCourse[4 + i].NomCourse + "\n Distance \n"
-            + ListeOBJETCourse[4 + i].Distance + "\n DateHeure: \n" + ListeOBJETCourse[4 + i].DateDepart.Day + "/" + ListeOBJETCourse[4 + i].DateDepart.Month + "/" + ListeOBJETCourse[4 + i].DateDepart.Year +
-            "\n Heure depart: \n " + ListeOBJETCourse[4 + i].HeureDepartCourse.Hour + ":" + ListeOBJETCourse[4 + i].HeureDepartCourse.Minute + "\n Prix: \n" +
-            ListeOBJETCourse[4 + i].PrixInscriptionCourse;
+            LabelCourse5.Content = ListeOBJETCourse[0 + i].NomCourse;
+            DistanceCourse5.Content = "\n Distance : \n" + ListeOBJETCourse[0 + i].Distance;
+            DateCourse5.Content = "\n Date de départ : \n" + ListeOBJETCourse[0 + i].DateDepart.Day +
+            "/" + ListeOBJETCourse[0 + i].DateDepart.Month + "/" + ListeOBJETCourse[0 + i].DateDepart.Year;
+            HeureCourse5.Content = "\n Heure depart : \n " + ListeOBJETCourse[0 + i].HeureDepartCourse.Hour + ":"
+            + ListeOBJETCourse[0 + i].HeureDepartCourse.Minute;
+            PrixCourse5.Content = "\n Prix: \n" +
+            ListeOBJETCourse[0 + i].PrixInscriptionCourse;
 
             LabelListeCourse.Content = ListeOBJETCourse[0 + i].NomCourse;
 
@@ -117,6 +137,56 @@ namespace WPF_SAE2._01
             radioChoisir2.IsChecked = false;
             radioChoisir3.IsChecked = false;
             radioChoisir4.IsChecked = false;
+        }
+        public bool ContientMotCleNom(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(TBSelNom.Text))
+                return true;
+            else
+                return (unCoureur.NomCoureur.StartsWith(TBSelNom.Text, StringComparison.OrdinalIgnoreCase));
+
+        }
+        public bool ContientMotCleVille(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(TBSelVille.Text))
+                return true;
+            else
+                return (unCoureur.VilleCoureur.StartsWith(TBSelVille.Text, StringComparison.OrdinalIgnoreCase));
+
+        }
+        private void TBSelNom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            DataGridCoureur.Items.Filter = ContientMotCleNom;
+            CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
+
+        }
+
+        private void TBSelVille_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            DataGridCoureur.Items.Filter = ContientMotCleVille;
+            CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
+
+        }
+
+        public bool ContientMotCleFederation(object obj)
+        {
+            CoureurClasse unCoureur = obj as CoureurClasse;
+            if (String.IsNullOrEmpty(ComboBoxFederation.Text))
+                return true;
+            else
+                return (unCoureur.Federation.Equals(int.Parse(ComboBoxFederation.Text)));
+
+        }
+        private void ComboBoxFederation_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+
+
+            DataGridCoureur.Items.Filter = ContientMotCleFederation;
+            CollectionViewSource.GetDefaultView(DataGridCoureur.ItemsSource).Refresh();
+
         }
     }
 }
